d253d7939743bb8b161a01198d6917f0676b4a74
Rayleigh47 <yanming8870@outlook.com>
2023-02-10 17:21:30 +0800

    changes

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,9 +1,9 @@
 package circus;
 
-import circus.animal.Animal;
-import circus.animal.Duck;
-import circus.animal.Parrot;
-import circus.animal.Tiger;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import circus.animal.*;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
@@ -41,8 +41,29 @@ public class Circus {
     }
 
     public static void main(String[] args) {
+        System.out.println("Number of animals: " + animals.length);
         makeAnimalsTalk();
         System.out.println("Total value of animals " + calculateAssetValue(animals));
         System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+
+//        animals[3] = new Parrot("Dolly");
+//        System.out.println("Number of animals: " + animals.length);
+        ArrayList <Animal> animalArray = new ArrayList<>(Arrays.asList(animals));
+        animalArray.add(new Duck("Louie"));
+        Parrot andy = new Parrot("Andy");
+        Elephant strongOne = new Elephant("strong one");
+        animalArray.add(andy);
+        animalArray.add(strongOne);
+        printAllAnimals(animalArray);
+        animalArray.sort(Animal.AnimalNameComparator);
+        printAllAnimals(animalArray);
+        System.out.println("Position of andy: " + animalArray.indexOf(andy));
     }
+
+    private static void printAllAnimals(ArrayList<Animal> animalArray) {
+        for(Animal a: animalArray) {
+            System.out.println(a);
+        }
+    }
+
 }
